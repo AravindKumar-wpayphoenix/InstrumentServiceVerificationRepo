460fc62564b2746db37ee1b40b7b49edfe4c2e5c
AravindKumar-wpayphoenix <agunasekaran@woolworths.com.au>
2022-08-12 17:03:17 +1000

    initial commit

--- src/test/java/testRunner/getInstrumentService.java
+++ src/test/java/testRunner/getInstrumentService.java
@@ -14,13 +14,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+import javax.net.ssl.*;
 import java.io.IOException;
 import java.security.InvalidKeyException;
+import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.time.Instant;
 
 public class getInstrumentService extends signatureGeneratorUtil
@@ -30,6 +29,7 @@ public class getInstrumentService extends signatureGeneratorUtil
    public void InstrumentService() throws NoSuchAlgorithmException, InvalidKeyException, IOException {
        String signature = signatureUtilGenerator();
        String KeyId= getGlobalValue("UAT");
+        disableSSLVerification();
        Response res= given()
                .config(RestAssured.config().sslConfig(
                        new SSLConfig().allowAllHostnames()))
@@ -46,6 +46,40 @@ public class getInstrumentService extends signatureGeneratorUtil
         int statusCode = response.getStatusCode();
        assertEquals(statusCode,200);
 
+    }
+
+    public void disableSSLVerification(){
+        try {
+            // Create a trust manager that does not validate certificate chains
+            TrustManager[] trustAllCerts = new TrustManager[] {
+                    new X509TrustManager() {
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return null;
+                        }
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                        }
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                        }
+                    }
+            };
+
+            // Install the all-trusting trust manager
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            // Create all-trusting host name verifier
+            HostnameVerifier allHostsValid = new HostnameVerifier() {
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            };
+
+            // Install the all-trusting host verifier
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            e.printStackTrace();
+        }
     }
 
 }
